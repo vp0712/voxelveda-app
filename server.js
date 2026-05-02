5aa680ab421cd6bc53d088b09a0408424db84dc4
Viral Vekariya <viralvekariya782@gmail.com>
2026-05-02 21:03:43 +1000

    fix railway server binding

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
