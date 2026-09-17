cc96a5cd1538e93af5c955bb34cd39a1150c9693
vp0712 <160989824+vp0712@users.noreply.github.com>
2026-09-17 16:19:02 +1000

    Add protected recovery assurance endpoint

--- routes/readinessRoutes.js
+++ routes/readinessRoutes.js
@@ -5,7 +5,10 @@ const { rateLimit } = require('../middleware/securityMiddleware');
 
 const router = express.Router();
 const readinessLimit = rateLimit({ windowMs: 60 * 1000, max: 60, keyPrefix: 'readiness-detail' });
+const recoveryLimit = rateLimit({ windowMs: 60 * 1000, max: 20, keyPrefix: 'recovery-assurance' });
+const securityAccess = requireAnyPermission('MANAGE_SECURITY', 'VIEW_SECURITY_GOVERNANCE');
 
-router.get('/', requireAnyPermission('MANAGE_SECURITY', 'VIEW_SECURITY_GOVERNANCE'), readinessLimit, controller.details);
+router.get('/', securityAccess, readinessLimit, controller.details);
+router.get('/recovery', securityAccess, recoveryLimit, controller.recovery);
 
 module.exports = router;
